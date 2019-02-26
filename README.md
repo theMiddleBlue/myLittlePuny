01c46afd27e79173cf99513eb03e3b10f86365da
theMiddleBlue <theMiddleBlue@users.noreply.github.com>
2019-02-26 21:28:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# My Little Puny
+A little and ugly Python script that checks for domain names that may be used to perform an (IDN homograph attack)[https://en.wikipedia.org/wiki/IDN_homograph_attack].
+
+### Punycode From Wikipedia:
+> Punycode is a representation of Unicode with the limited ASCII character subset used for Internet host names.
+> Using Punycode, host names containing Unicode characters are transcoded to a subset of ASCII consisting of letters,
+> digits, and hyphen, which is called the Letter-Digit-Hyphen (LDH) subset. For example, München (German name for Munich) 
+> is encoded as Mnchen-3ya.
+
+> While the Domain Name System (DNS) technically supports arbitrary sequences of octets in domain name labels, 
+> the DNS standards recommend the use of the LDH subset of ASCII conventionally used for host names, and require 
+> that string comparisons between DNS domain names should be case-insensitive. The Punycode syntax is a method 
+> of encoding strings containing Unicode characters, such as internationalized domain names (IDNA), into the LDH subset of 
+> ASCII favored by DNS. It is specified in IETF Request for Comments 3492.
+
+## Usage
+This script has 2 main arguments. The first argument is a capital letter from A to Z which is used to loops through a list of similar/compatible Unicode characters and convert it to IDN format using Punycode.
+The second argument is a "QNAME template" that has an underscore `_` instead of the character you want to check. For Example:
+
+```bash
+$ python myLittlePuny.py I tw_tter.com
+qname=xn--twtter-xva.com, decoded=twìtter.com, rtype=2, rdata=ns12.domaincontrol.com.
+qname=xn--twtter-xva.com, decoded=twìtter.com, rtype=2, rdata=ns11.domaincontrol.com.
+qname=xn--twtter-4va.com, decoded=twítter.com, rtype=2, rdata=ns12.domaincontrol.com.
+qname=xn--twtter-4va.com, decoded=twítter.com, rtype=2, rdata=ns11.domaincontrol.com.
+qname=xn--twtter-cwa.com, decoded=twîtter.com, rtype=2, rdata=dns111.ovh.net.
+qname=xn--twtter-cwa.com, decoded=twîtter.com, rtype=2, rdata=ns111.ovh.net.
+qname=xn--twtter-jwa.com, decoded=twïtter.com, rtype=2, rdata=ns12.domaincontrol.com.
+qname=xn--twtter-jwa.com, decoded=twïtter.com, rtype=2, rdata=ns11.domaincontrol.com.
+qname=xn--twtter-j8a.com, decoded=twītter.com, rtype=2, rdata=ns06.domaincontrol.com.
+qname=xn--twtter-j8a.com, decoded=twītter.com, rtype=2, rdata=ns05.domaincontrol.com.
+qname=xn--twtter-cl8b.com, decoded=twịtter.com, rtype=2, rdata=ns1.transip.nl.
+qname=xn--twtter-cl8b.com, decoded=twịtter.com, rtype=2, rdata=ns2.transip.eu.
+qname=xn--twtter-cl8b.com, decoded=twịtter.com, rtype=2, rdata=ns0.transip.net.
+...
+```
+
+The default QTYPE is NS but you can change it using `--qtype`:
+
+```bash
+$ python myLittlePuny.py I tw_tter.com --qtype=A
+qname=xn--twtter-xva.com, decoded=twìtter.com, rtype=1, rdata=78.153.209.228
+qname=xn--twtter-4va.com, decoded=twítter.com, rtype=1, rdata=78.153.209.228
+qname=xn--twtter-cwa.com, decoded=twîtter.com, rtype=1, rdata=213.186.33.87
+qname=xn--twtter-jwa.com, decoded=twïtter.com, rtype=1, rdata=78.153.209.228
+qname=xn--twtter-j8a.com, decoded=twītter.com, rtype=1, rdata=184.168.221.43
+qname=xn--twtter-cl8b.com, decoded=twịtter.com, rtype=1, rdata=37.97.254.27
+```
+
+## Requirements
+- python3
+- dnslib
+
+## Install and Run
+```bash
+$ # install required python modules:
+$ pip3 install -r requirements.txt
+
+$ # run myLittlePuny:
+$ python3 myLittlePuny.py --help
+```
+
+
---
+++ myLittlePuny.py
@@ -0,0 +1,144 @@
+import sys, time, socket, re, argparse, codecs
+from dnslib import *
+
+debug = False
+version = "0.1"
+
+def inlineopts():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument(	"capital",
+								metavar="LETTER",
+								type=str,
+								nargs='+',
+								help="Capital letter [A-Z]")
+	parser.add_argument(	"template",
+								metavar="TEMPLATE", 
+								type=str, 
+								nargs='+', 
+								help="Domain name template (example: g_oogle.com)")
+	parser.add_argument(	"--qtype",
+								help='QTYPE of DNS query (default: NS)',
+								type=str,
+								required=False)
+	parser.add_argument(	"--resolver-ip",
+								help='DNS Server to use for query (default: 8.8.8.8)',
+								type=str)
+	parser.add_argument(	"--resolver-port",
+								help='Port of the DNS resolver (default: 53)',
+								type=int)
+	parser.add_argument(	"--verbose",
+								action="store_true",
+								dest="verbose",
+								default=False,
+								help='Verbose (default: off)')
+
+	return parser.parse_args()
+
+
+opt = inlineopts()
+
+dns_resolver_ip = opt.resolver_ip or '8.8.8.8'
+dns_resolver_port = opt.resolver_port or 53
+dns_qtype = opt.qtype or "NS"
+
+class cc:
+	HEA = '\033[95m'
+	BLU = '\033[94m'
+	GRN = '\033[92m'
+	WRN = '\033[93m'
+	FAL = '\033[91m'
+	ECL = '\033[0m'
+	BLD = '\033[1m'
+	UND = '\033[4m'
+
+try:
+	unichr
+except NameError:
+	unichr = chr
+
+def unichar(i):
+	try:
+		return unichr(i)
+	except ValueError:
+		return struct.pack('i', i).decode('utf-32')
+
+def qname_decode(char, template=opt.template[0]):
+	c = unichar(int(char.strip(), 16))
+	return str(template).replace('_', str(c))
+	# return str(template).replace('_', str(c))
+
+def qname_encode(char, template=opt.template[0]):
+	c = unichar(int(char.strip(), 16))
+	return str(template).replace('_', str(c)).encode("idna")
+	# return str(template).replace('_', str(c)).encode("idna")
+
+def make_query(qname, qtype="NS"):
+	return DNSRecord.question(qname, qtype)
+
+def send_query(q, daddr="8.8.8.8", dport=53):
+	UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+	UDPClientSocket.settimeout(10)
+	UDPClientSocket.sendto(q.pack(), (daddr, dport) )
+	recvdata = UDPClientSocket.recvfrom(1024)[0]
+
+	return DNSRecord.parse(recvdata)
+
+def parse_answer(qname, decoded_qname, rr):
+	answ = ''
+	for i in rr:
+		sys.stdout.write("\r\033[K")
+		answ = str(i.rdata)
+		print('qname='+cc.GRN+str(qname)+cc.ECL+', decoded='+cc.BLU+decoded_qname+cc.ECL+', rtype='+str(i.rtype)+', rdata='+cc.HEA+answ+cc.ECL)
+
+
+qlist = {}
+with open("UnicodeData.txt") as f:
+	l = f.readlines()
+	for i in l:
+		if re.search('^[0-9a-fA-F]{4,5};.*SMALL LETTER '+opt.capital[0]+'.*', i.strip()) is not None:
+
+			linearr = i.strip().split(";")
+			char = linearr[0]
+
+			decoded_qname = qname_decode(char)
+
+			try:
+				qname = qname_encode(char)
+			except:
+				if debug is True:
+					print('Warning: Unable to encode '+decoded_qname)
+					continue
+
+			qlist[qname] = decoded_qname
+
+if opt.verbose is True:
+	print(" -")
+	print("| Version: "+version)
+	print("| QNAME List length: "+ str(len(qlist)))
+	print("| Compatible unicode: "+str(opt.capital[0]))
+	print("| QNAME template: "+str(opt.template[0]))
+	print(" -\n")
+
+for qname,decoded_qname in qlist.items():
+	if True:
+		if True:
+			if re.search('xn\-\-', str(qname)) is not None:
+				sys.stdout.write('Trying to resolve '+str(qname)+' ('+decoded_qname+')...')
+				sys.stdout.flush()
+
+				q = make_query(qname, qtype=dns_qtype)
+
+				try:
+					d = send_query(q, daddr=dns_resolver_ip, dport=53)
+
+				except Exception as e:
+					print(" <- Warning: "+str(e))
+
+				parse_answer(qname, decoded_qname, d.rr)
+
+				# time.sleep(0.5)
+				sys.stdout.write("\r\033[K")
+			else:
+				if debug is True:
+					print("Info: ignoring "+str(qname))
